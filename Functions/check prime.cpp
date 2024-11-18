cb59835107811f512c5e7710ea25cd294510a809
surajmendhe5573 <surajmendhe470@gmail.com>
2024-11-18 21:24:49 +0530

    check prime added

---
+++ Functions/check prime.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void checkPrime(int n){
+    bool isPrime= true;
+
+    for(int i=2;i<=n-1; i++){
+        if(n%i == 0){
+            isPrime= false;
+            break;
+        }
+    }
+    if(isPrime){
+        cout<<"Prime Number";
+    }else{
+        cout<<"Not Prime";
+    }
+}
+int main(){
+    checkPrime(7);
+
+    return 0;
+}
