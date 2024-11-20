bc7817dbf9ce86e90a49b2ba9d2f5851ee810997
surajmendhe5573 <surajmendhe470@gmail.com>
2024-11-20 16:39:35 +0530

    changes

--- arrays.cpp
+++ arrays.cpp
@@ -1,12 +1,36 @@
-// Print an array.
+// // Print an array.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+
+//     int marks[5]= {99, 76, 88, 98, 87};
+
+//     for(int i=0;i<5; i++){
+//         cout<<marks[i]<<" ";
+//     }
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
+    int n;
+    cout<<"Enter a number of array elements: ";
+    cin>>n;
 
-    int marks[5]= {99, 76, 88, 98, 87};
+    int arr[n];
 
-    for(int i=0;i<5; i++){
-        cout<<marks[i]<<" ";
+    cout<<"Enter the array elements: "<<endl;
+    for(int i=0;i<n; i++){
+        cin>>arr[i];
     }
+
+   cout<<"These are array elemenets: "<<endl;
+   for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+   }
+
     return 0;
+
 }
